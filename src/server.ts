91d46107d134a85f6f93118fe635834c970bbb44
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-05-18 12:15:58 -0300

    rota de feedback

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.send("Hello world")
+app.use(express.json())
+
+app.post('/feedbacks', async (req, res) => {
+  // console.log(req.body)
+  const { type, comments, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comments,
+      screenshot
+    }
+  })
+
+
+  return res.status(201).json(feedback)
 })
 
 app.listen(3333, () => {
